f4d8b3f73c84e2377b9cb4e3fedf9f4f0bab7aa7
Andrew Davis <atd285@gmail.com>
2019-11-02 20:55:39 -0400

    api for enabling channels created

--- server/src/api/channel.js
+++ server/src/api/channel.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const channelModel = require('../db/channel');
+router.patch('/:twitchId', async (req, res, next) => {
+  //this endpoint will be for updating a channel with a given id
+  const { twitchId } = req.params;
+  const { enabled } = req.body;
+  //Todo: check manager collection too, instead of just id
 
-// router.patch('/:twitchId', (req, res) => {
-//   const { id } = req.query;
-// });
+  if (enabled === undefined || typeof enabled !== 'boolean') {
+    return next(new Error('Enabled must be of type: boolean'));
+  }
+  try {
+    const channel = await channelModel.findOneAndUpdate(
+      { twitchId },
+      { enabled },
+      { new: true },
+    );
+
+    if (!channel) {
+      return next();
+    }
+    res.json(channel);
+  } catch (error) {
+    return next(error);
+  }
+  if (twitchId !== req.user.twitchId) {
+    const error = new Error('now allowed!');
+    return next(error);
+  }
+});
 
 module.exports = router;
--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,38 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-const path = require('path');
 const morgan = require('morgan');
+const middleware = require('./middleware');
 require('dotenv').config();
 const config = require('./config');
 require('./db');
 const app = express();
+app.use(cors());
+app.use(helmet());
 app.use(morgan('combined'));
-
+app.use(middleware.decodeAuthHeader);
+app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ hello: 'world!' });
 });
 app.use('/auth/twitch', require('./auth/twitch'));
+app.use('/api/channel', require('./api/channel'));
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found - ' + req.originalUrl);
+  res.status(404);
+  next(error);
+});
+
+//eslint-disable-next-line
+app.use((error, req, res, next) => {
+  res.status(res.statusCode === 200 ? 500 : res.statusCode);
+  res.json({
+    message: error.message,
+    stack: config.NODE_ENV === 'production' ? undefined : error.stack,
+  });
+});
 
 const port = config.PORT;
 
